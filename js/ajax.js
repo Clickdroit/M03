b0de17af53c91a26419b7cf432452bb27464a554
Maxime <1erfr2007@gmail.com>
2025-12-04 16:35:40 +0100

    Add HTML structure for capteur, lumiere, and prise pages; update AJAX and app.js for navigation and weather display

--- js/ajax.js
+++ js/ajax.js
@@ -1,24 +1,41 @@
-function ajax(){
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-            var reponse=this.responseText;
-            var reponseMeteo= JSON.parse(reponse);
-            
-            var temp=reponseMeteo.main.temp + " °C";
-            document.getElementById("temperature").innerHTML=temp;
-            var ville="Ville: "+reponseMeteo.name;
-            document.getElementById("ville_meteo").innerHTML=ville;
-            var icone=reponseMeteo.weather[0].icon;
-            document.getElementById("icone_meteo").src="https://openweathermap.org/img/wn/"+icone+"@2x.png";
-            
+function ajax() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var reponse = this.responseText;
+      var reponseMeteo = JSON.parse(reponse);
 
-            console.log(icone);
-            console.log(temp);
-            console.log(ville);
-        }
-    };
-    xhttp.open("GET", "https://api.openweathermap.org/data/2.5/weather?units=metric&lat=48.8575&lon=2.3514&exclude=hourly,daily&appid=d85e295dccdc11e63077777de976518f");
-    xhttp.send();
+      var temp = reponseMeteo.main.temp + " °C";
+      document.getElementById("temperature").innerHTML = temp;
+      var ville = "Ville: " + reponseMeteo.name;
+      document.getElementById("ville_meteo").innerHTML = ville;
+      var icone = reponseMeteo.weather[0].icon;
+      document.getElementById("icone_meteo").src =
+        "https://openweathermap.org/img/wn/" + icone + "@2x.png";
+
+      console.log(icone);
+      console.log(temp);
+      console.log(ville);
+    }
+  };
+  xhttp.open(
+    "GET",
+    "https://api.openweathermap.org/data/2.5/weather?units=metric&lat=48.8575&lon=2.3514&exclude=hourly,daily&appid=d85e295dccdc11e63077777de976518f"
+  );
+  xhttp.send();
 }
 ajax();
+
+function AfficherLumiere() {
+  const xhttpAL = new XMLHttpRequest();
+  xhttpAL.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var reponseAL = this.responseText;
+
+      console.log(reponseAL);
+    }
+  };
+  xhttpAL.open("GET", "http://172.20.21.230/api/F163AB8D21/lights/");
+  xhttpAL.send();
+}
+AfficherLumiere();
--- js/app.js
+++ js/app.js
@@ -1,15 +1,43 @@
 var logoHamburger = document.getElementById("logoHamburger");
 var nav = document.getElementById("navigator");
-var fermer = document.getElementById('close');
-fermer.addEventListener('click', FermerMenu)
-logoHamburger.addEventListener('click', AfficherMenu);
+var fermer = document.getElementById("closeNav");
+fermer.addEventListener("click", FermerMenu);
+logoHamburger.addEventListener("click", AfficherMenu);
+document.getElementById("homeNAV").addEventListener("click", AfficherAccueil);
+document
+  .getElementById("lumiereNAV")
+  .addEventListener("click", AfficherLumiere);
+document.getElementById("priseNAV").addEventListener("click", AfficherPrise);
+document
+  .getElementById("capteurNAV")
+  .addEventListener("click", AfficherCapteur);
 
-function AfficherMenu(){
-    document.getElementById('navigator').style.display='block';
+function AfficherMenu() {
+  document.getElementById("navigator").style.display = "block";
+  ajax();
 }
-function FermerMenu(){
-    document.getElementById('navigator').style.display='none';
+function FermerMenu() {
+  document.getElementById("navigator").style.display = "none";
 }
-
+function AfficherAccueil() {
+  window.location.href = "index.html";
+}
+function AfficherLumiere() {
+  window.location.href = "lumiere.html";
+  document.getElementById("navigator").style.display = "none";
+  var section = document.getElementById("section");
+  section.innerHTML = "";
+  var lumiere = "";
+  lumiere =
+    lumiere +
+    '<img src="images/lightbulb.png" alt="lumiere"><button id="on">ON</button><button id="off">OFF</button>';
+  section.innerHTML = lumiere;
+}
+function AfficherPrise() {
+  window.location.href = "prise.html";
+}
+function AfficherCapteur() {
+  window.location.href = "capteur.html";
+}
 
 //d85e295dccdc11e63077777de976518f API KEY
