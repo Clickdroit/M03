c54fdf0b3782a3d543765964f0eb3eba3439b525
Maxime <1erfr2007@gmail.com>
2025-12-11 16:56:39 +0100

    Refactor AfficherLumiere and AfficherPrise functions to improve dynamic light and socket display; replace inline HTML generation with event-driven button actions

--- js/ajax.js
+++ js/ajax.js
@@ -29,21 +29,6 @@ ajax();
 var lumiereNav = document.getElementById("lumiereNAV");
 lumiereNav.addEventListener("click", AfficherLumiere);
 
-function ajaxLumiere() {
-  FermerMenu();
-  document.getElementById("section").innerHTML = "";
-  var lumiere = '';
-  lumiere = lumiere + '<div class="lumiere-card">';
-  lumiere = lumiere + "<h3>" + item.name + "</h3>";
-  lumiere =lumiere + '<img src="images/lightbulb.png" alt="lumiere">';
-  lumiere =lumiere + '<button class="btn-on" id="'+uniqueid+'">ON</button>';           
-  lumiere =lumiere +'<button class="btn-off" id="'+uniqueid+'">OFF</button>';
-  lumiere = lumiere + "</div>";
-  document.getElementById("section").innerHTML = lumiere;
-}
-
-
-
 function AfficherLumiere() {
   FermerMenu();
   const xhttpAL = new XMLHttpRequest();
@@ -65,9 +50,9 @@ function AfficherLumiere() {
         if (item.type && item.type.toLowerCase().includes("light")) {
           lumiere = lumiere + '<div class="lumiere-card">';
           lumiere = lumiere + "<h3>" + item.name + "</h3>";
-          lumiere =lumiere + '<img src="images/lightbulb.png" alt="lumiere">';
-          lumiere =lumiere + '<button class="btn-on" id="'+uniqueid+'">ON</button>';           
-          lumiere =lumiere +'<button class="btn-off" id="'+uniqueid+'">OFF</button>';
+          lumiere = lumiere + '<img src="images/lightbulb.png" alt="lumiere">';
+          lumiere = lumiere + '<button class="btn-on" onclick="AllumerLumiere(' + num + ')">ON</button>';           
+          lumiere = lumiere + '<button class="btn-off" onclick="EteindreLumiere(' + num + ')">OFF</button>';
           lumiere = lumiere + "</div>";
         }
       }
@@ -77,6 +62,85 @@ function AfficherLumiere() {
   xhttpAL.open("GET", "http://172.20.21.230/api/F163AB8D21/lights/");
   xhttpAL.send();
 }
-if (window.location.pathname.includes("lumiere.html")) {
-  AfficherLumiere();
+function AllumerLumiere(num) {
+  const xhttpOn = new XMLHttpRequest();
+  xhttpOn.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log("Lumiere " + num + " allumée");
+    }
+  };
+  xhttpOn.open("PUT", "http://172.20.21.230/api/F163AB8D21/lights/" + num + "/state");
+  xhttpOn.send(JSON.stringify({"on": true}));
+}
+
+function EteindreLumiere(num) {
+  const xhttpOff = new XMLHttpRequest();
+  xhttpOff.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log("Lumiere " + num + " éteinte");
+    }
+  };
+  xhttpOff.open("PUT", "http://172.20.21.230/api/F163AB8D21/lights/" + num + "/state");
+  xhttpOff.send(JSON.stringify({"on": false}));
+}
+
+
+var priseNav = document.getElementById("priseNAV");
+priseNav.addEventListener("click", AfficherPrise);
+
+
+function AfficherPrise() {
+  FermerMenu();
+  const xhttpAL = new XMLHttpRequest();
+  xhttpAL.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var reponseAL = this.responseText;
+      var prises = JSON.parse(reponseAL);
+
+      console.log(reponseAL);
+      var section = document.getElementById("section");
+      section.innerHTML = "";
+      var prise = '';
+      for (var num in prises) {
+        var item = prises[num];
+        let name = item.name;
+        let type = item.type;
+        let etat = item.state.on; 
+        let uniqueid = item.uniqueid;
+        console.log("ID: " + num + " Type: " + type + " Etat: " + etat + " UniqueID: " + uniqueid + "name : "+name);
+        if (item.type && item.type.toLowerCase().includes("ZHATemperature")) {
+          prise = prise + '<div class="prise-card">';
+          prise = prise + "<h3>" + item.name + "</h3>";
+          prise = prise + '<img src="images/lightbulb.png" alt="prise">';
+          prise = prise + '<button class="btn-on" onclick="Allumerprise(' + num + ')">ON</button>';           
+          prise = prise + '<button class="btn-off" onclick="Eteindreprise(' + num + ')">OFF</button>';
+          prise = prise + "</div>";
+        }
+      }
+      section.innerHTML = prise;
+    }
+  };
+  xhttpAL.open("GET", "http://172.20.21.230/api/F163AB8D21/sensors/");
+  xhttpAL.send();
+}
+function Allumerprise(num) {
+  const xhttpOn = new XMLHttpRequest();
+  xhttpOn.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log("Prise " + num + " allumée");
+    }
+  };
+  xhttpOn.open("PUT", "http://172.20.21.230/api/F163AB8D21/sensors/" + num + "/state");
+  xhttpOn.send(JSON.stringify({"on": true}));
+}
+
+function Eteindreprise(num) {
+  const xhttpOff = new XMLHttpRequest();
+  xhttpOff.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log("Prise " + num + " éteinte");
+    }
+  };
+  xhttpOff.open("PUT", "http://172.20.21.230/api/F163AB8D21/sensors/" + num + "/state");
+  xhttpOff.send(JSON.stringify({"on": false}));
 }
